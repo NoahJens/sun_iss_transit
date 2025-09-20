a30e3e4610436dbfd2331485cc78670eb191d7fe
NoahJens <139884694+NoahJens@users.noreply.github.com>
2025-09-20 21:16:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from skyfield.api import wgs84
 from astro_data import earth, sun, iss, epoch 
 from transit import find_transit 
-from utils import decimal_places
+from .utils import decimal_places
 
 # # Define observation point
 # observer = earth + wgs84.latlon(53.788419, 9.569346) # Sommerland 
