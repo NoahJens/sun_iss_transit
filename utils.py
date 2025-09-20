cb093482a5ff40eed61881231e38f203e5789692
NoahJens <139884694+NoahJens@users.noreply.github.com>
2025-09-20 14:26:59 +0200

    Initial commits

---
+++ utils.py
@@ -0,0 +1,28 @@
+from skyfield.api import load, Topos, EarthSatellite, Angle, wgs84
+import pytz
+from skyfield.timelib import Time
+from datetime import datetime
+
+def angular_separation(t, observer, sun, iss):
+    """Angular separation (degrees) between Sun and ISS as seen by observer"""
+    sun_vec = observer.at(t).observe(sun).apparent()
+    iss_vec = observer.at(t).observe(iss).apparent()
+
+    sun_alt, sun_az, sun_dist = sun_vec.altaz()
+    sun_alt = sun_alt.degrees
+    
+    return sun_vec.separation_from(iss_vec).degrees, sun_alt
+
+def convert_t(t):
+    """
+    Convert a Skyfield Time or a datetime object to Berlin time and format as string.
+    """
+    if isinstance(t, Time):  # Skyfield Time object
+        t_utc = t.utc_datetime()
+    elif isinstance(t, datetime):  # regular datetime
+        t_utc = t
+    else:
+        raise TypeError("t must be a Skyfield Time or datetime object")
+
+    t_berlin = t_utc.astimezone(pytz.timezone('Europe/Berlin'))
+    return t_berlin.strftime("%Y-%m-%d %H:%M:%S %Z")
