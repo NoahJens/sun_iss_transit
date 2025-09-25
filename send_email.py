4c0e96be2f4e1077053283fe6f83297b973db09d
NoahJens <139884694+NoahJens@users.noreply.github.com>
2025-09-25 15:08:37 +0200

    write independent transit calculation for email

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
-from skyfield.api import wgs84
-from astro_data import earth, sun, load_iss_data
+from skyfield.api import EarthSatellite, wgs84, load
+
+from astro_data import earth, sun
 from transit import find_transit 
 from datetime import datetime
 
@@ -9,10 +10,49 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 import os
+import requests 
+from utils import convert_t
+import time
+
+time.sleep(10)
+
+ts = load.timescale()
+
+planets = load('de421.bsp')
+earth, sun = planets['earth'], planets['sun']
+observer = earth + wgs84.latlon(53.7985, 9.5470)
+
+owner = "NoahJens"
+repo = "sun_iss_transit"
+file_path = "ISS.csv"
+branch = "main"
+workflow_file = "TLE_download.yml"
+headers = {"Authorization": f"token {st.secrets['GITHUB_TOKEN']}"}
+url_runs = f"https://api.github.com/repos/{owner}/{repo}/actions/workflows/{workflow_file}/runs"
+params = {"branch": "main", "per_page": 1}
+
+# Load ISS position from online repo
+url_commit = f"https://api.github.com/repos/{owner}/{repo}/commits?path={file_path}&sha={branch}"
+r = requests.get(url_commit)
+r.raise_for_status()
+latest_commit_sha = r.json()[0]["sha"]
+
+# Fetch CSV at that commit
+url_raw_commit = f"https://raw.githubusercontent.com/{owner}/{repo}/{latest_commit_sha}/{file_path}"
+r = requests.get(url_raw_commit)
+r.raise_for_status()
+data = r.json()
+
+# Find the ISS row (using NORAD ID is safest)
+iss_row = next(row for row in data if row.get("NORAD_CAT_ID") == 25544)
+
+# Create the EarthSatellite object
+iss_geo = EarthSatellite.from_omm(ts, iss_row) # gets the geocentric information on the iss
+epoch = convert_t(iss_geo.epoch)
+
+iss = earth + iss_geo
 
 # Calculate transits 
-iss, epoch = load_iss_data(override = True)
-observer = earth + wgs84.latlon(53.7985, 9.5470)
 transit = find_transit(observer, sun, iss)
 transit["Epoch"] = epoch
 transit.to_csv("transits.csv", index=False)
